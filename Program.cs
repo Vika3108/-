a970aedcbe741b2fe7e834756f53a9784f399ffa
vika3108 <vika.antonova1997@gmail.com>
2022-10-06 23:15:26 +0300

    решили шестую задачу

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+/* Вторая задача
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+    Console.WriteLine("{0} наибольшее число", number1);
+else 
+     Console.WriteLine("{0} наибольшее число ", number2);
+        Console.WriteLine("{0} наименьшее число ", number1);
+
+*/
+/* Четвертая задача
+Console.WriteLine("Введите 3 числа:");
+        int a = Int32.Parse(Console.ReadLine());
+        int b = Int32.Parse(Console.ReadLine());
+        int c = Int32.Parse(Console.ReadLine());
+ 
+        int maximal = Math.Max(Math.Max(a, b), c);
+        int minimal = Math.Min(Math.Min(a, b), c);
+ 
+        Console.WriteLine("{0} -> {1} -> {2}",
+            minimal,
+            a + b + c - maximal - minimal,
+            maximal);
+*/
+/* Шестая задача
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0)
+Console.WriteLine(" Введенное число является четным");
+else
+Console.WriteLine("Введенное число является нечетным"); 
+*/
+
+
